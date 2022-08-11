869630d76ebc0c33058af65bd36f09305c23c09c
lamv <tienlam2001@gmail.com>
2022-08-10 23:48:53 -0400

    de

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
@@ -19,8 +18,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
+
+app.get('/',(req,res)=>{
+  res.send('Dede')
+})
+
+
+
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.sendFile(path.join(__dirname,'/views/homePage.html'));
 });
 
 module.exports = router;
