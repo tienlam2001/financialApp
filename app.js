92c0427fc790002d4b485c8d68ef5d3a072a649a
lamv <tienlam2001@gmail.com>
2022-08-10 23:22:04 -0400

    de

--- app.js
+++ app.js
@@ -9,15 +9,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(express.static('public'))
-
-
-
-
-
-
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
